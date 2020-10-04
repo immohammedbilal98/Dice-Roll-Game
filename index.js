c05d6a5434fa50ad854a6d47be832182f711a5ce
Mohammad Bilal <49686277+immohammedbilal98@users.noreply.github.com>
2020-10-04 18:57:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//Player 1
+
+var randomNumber1 = Math.floor(Math.random()*6)+1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png-dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png-images/dice6.png
+
+var image1= document.querySelectorAll("img")[0]
+
+image1.setAttribute("src",randomImageSource);
+
+//Player 2
+
+var randomNumber2 = Math.floor(Math.random()*6)+1; //1-6
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; //images/dice1.png-images/dice6.png
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h3").innerHTML = "🎊 Player 1 is Winner 🎉";
+}
+else if (randomNumber1 < randomNumber2){
+  document.querySelector("h3").innerHTML = "🎊 Player 2 is Winner 🎉";
+}
+else {
+  document.querySelector("h3").innerHTML = "👉 Draw 👈";
+  document.querySelector("h3").innerHTML = "👉 Draw 👈";
+  }
